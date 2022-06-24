1c52aa5afea5ab63e85edcea584b36f401d0714d
elmermarquez09 <106798242+elmermarquez09@users.noreply.github.com>
2022-06-24 11:19:02 +0800

    Add files via upload

---
+++ EmployeeDriver.java
@@ -0,0 +1,11 @@
+//Marquez, Elmer Jr. C.
+//BSIT2-1
+public class EmployeeDriver {
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee();
+        System.out.println(emp);
+    }
+
+}
